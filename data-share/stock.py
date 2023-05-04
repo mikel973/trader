22daf1141af8711f4538edd45311d765358be410
mikel <mikel0lib7@gmail.com>
2023-05-04 14:32:29 +0800

    daily update

--- data-share/stock.py
+++ data-share/stock.py
@@ -1,6 +1,7 @@
 import tushare as ts
 import baostock as bs
 import pandas as pd
+import sqlite3
 
 from backtrader.utils import date2num
 from datetime import datetime
@@ -30,6 +31,7 @@ class DataStock:
         return daily
 
     # 历史A股K线数据
+    # http://baostock.com/baostock/index.php/Python_API%E6%96%87%E6%A1%A3
     # 股票代码，sh或sz.+6位数字代码，或者指数代码
     # adjustflag：复权类型，默认不复权：3；1：后复权；2：前复权
     # frequency：默认为d，日k线；d=日k线
@@ -78,6 +80,50 @@ def start():
     ds.stock_bs_minutes(code, '../test-data/' + 'min60-' + code + '.csv')
     return
 
+def saveBasic():
+    ds = DataStock("f8ab08eb9eb8223df1758fd93d42870820d74f29268d15ca9ee90a58")
+    basic = ds.stock_ts_basics('../test-data/stock_basic.csv')
+    # 连接到SQLite数据库
+    conn = sqlite3.connect('../test-data/example.db')
+    # 将DataFrame中的数据写入SQLite数据库中的新表'people'
+    basic.to_sql(name='basic', con=conn, if_exists='replace', index=False)
+    # 关闭数据库连接
+    conn.close()
+
+    return
+
+
+def getSymbol(symbol):
+    print("try to get symbol ", symbol)
+
+    # 连接到SQLite数据库
+    conn = sqlite3.connect('../test-data/example.db')
+
+    # 从数据库中读取数据到DataFrame
+    query = "SELECT symbol,exchange,list_date FROM basic WHERE symbol=?"
+    df = pd.read_sql_query(query, conn, params=[symbol])
+    # 关闭数据库连接
+    conn.close()
+
+    # 打印结果
+    print(df)
+
+    ds = DataStock("f8ab08eb9eb8223df1758fd93d42870820d74f29268d15ca9ee90a58")
+    # 遍历DataFrame并打印每一行
+    for index, row in df.iterrows():
+        code = row['symbol']
+        if row['exchange'] == 'SZSE':
+            code = 'sz.' + code
+        elif row['exchange'] == 'SSE':
+            code = 'sh.' + code
+
+        ds.stock_bs_daily(code, '../test-data/' + 'daily-' + code + '.csv')
+        ds.stock_bs_minutes(code, '../test-data/' + 'min60-' + code + '.csv')
+
+    return
+
 
 if __name__ == '__main__':
-    start()
+    # start()
+    saveBasic()
+    getSymbol('600000')
