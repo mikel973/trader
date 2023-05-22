436eaa27c5f49411cb41dfd0e0df89624d70f011
mikel <mikel0lib7@gmail.com>
2023-05-22 19:57:30 +0800

    update daily

--- data-share/okcoin.py
+++ data-share/okcoin.py
@@ -3,21 +3,20 @@ import datetime
 import pandas as pd
 import okx.MarketData as MarketData
 
-def start(db_name):
+def start(db_name, symbol, freq):
 
-
-    symbol = "BTC-USDT"
     api_key = ''
     api_secret_key = ''
     passphrase = ''
     # flag = "1"  # live trading: 0, demo trading: 1
     market_api = MarketData.MarketAPI(api_key, api_secret_key, passphrase, use_server_time=False, flag='1')
-    result = market_api.get_candlesticks(instId="BTC-USDT", bar="15m")
+    result = market_api.get_candlesticks(instId="BTC-USDT", bar=freq)
 
     k_data = result['data']
     col_name = ['datetime', 'open', 'high', 'low', 'close', 'vol', 'volCcy', 'volCcyQuote', 'confirm']
     k_data = pd.DataFrame(data=k_data, columns=col_name)
 
+    k_data['symbol'] = f"{symbol}-{freq}"
     k_data['date'] = pd.to_datetime(k_data['datetime'], unit='ms')
     k_data['date'] = k_data['date'].apply(lambda x: x.strftime('%Y-%m-%d %H:%M:%S'))
     k_data['open'] = k_data['open'].astype(float)
@@ -26,23 +25,25 @@ def start(db_name):
     k_data['close'] = k_data['close'].astype(float)
     k_data = k_data.set_index('datetime')
 
-    col_name = ['date', 'open', 'high', 'low', 'close', 'vol', 'volCcy', 'volCcyQuote', 'confirm']
+    col_name = ['date', 'open', 'high', 'low', 'close', 'vol', 'volCcy', 'volCcyQuote', 'confirm','symbol']
     k_data = k_data.reindex(columns=col_name)
 
     k_data.to_csv(str('./test.csv'))
 
-    # conn = sqlite3.connect(db_name)
-    # result = k_data.to_sql(name='daily', con=conn, if_exists='replace', index=False)
-    # if result is None:
-    #     print(f"### 追加{symbol}历史数据失败:{result}")
-    # else:
-    #     conn.commit()
-    #     print(f"追加{symbol}历史数据记录:{result}条.")
-    # # 关闭数据库连接
-    # conn.close()
+    conn = sqlite3.connect(db_name)
+    result = k_data.to_sql(name='daily', con=conn, if_exists='replace', index=False)
+    if result is None:
+        print(f"### 追加{symbol}历史数据失败:{result}")
+    else:
+        conn.commit()
+        print(f"追加{symbol}历史数据记录:{result}条.")
+    # 关闭数据库连接
+    conn.close()
 
     return
 
 if __name__ == '__main__':
     db_name = './okex.db'
-    start(db_name)
+    symbol = 'BTC-USDT'
+    freq = '15m'
+    start(db_name, symbol, freq)
